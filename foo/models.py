e437534b3c88311d07c970579588cfd6d7463d4f
David Rae <david.rae@vmsuk.com>
2013-07-05 17:02:37 +0100

    first commit

---
+++ foo/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+class Foo(models.Model):
+	name = models.CharField(max_length=20, blank=True, null=True,
+		help_text="put your name here")
+	last_name = models.CharField(max_length=10)
